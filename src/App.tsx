97e9421e4e40a7d6526551806b1651c82da7b32a
Qaziaaaa <qazithekingston@gmail.com>
2026-04-28 15:35:53 +0500

    Restore net line background and make more prominent

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,10 @@ export default function App() {
   return (
     <Router>
       <div className="min-h-screen bg-black text-zinc-100 font-sans selection:bg-orange-500/30 selection:text-orange-200 relative">
-        {/* Global Grid Background - Made Prominent */}
+        {/* Global Grid Background - Net lines made more prominent */}
         <div className="fixed inset-0 z-0 pointer-events-none bg-black">
-          <div className="absolute inset-0 bg-[radial-gradient(#ffffff25_2px,transparent_2px)] bg-[size:32px_32px]"></div>
-          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-black/60 to-black"></div>
+          <div className="absolute inset-0 bg-[linear-gradient(to_right,#ffffff25_1px,transparent_1px),linear-gradient(to_bottom,#ffffff25_1px,transparent_1px)] bg-[size:40px_40px]"></div>
+          <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,transparent_0%,#000000_100%)]"></div>
         </div>
         
         <div className="relative z-10 flex flex-col min-h-screen">
